22da741b40bc9ab711bc9321a230e2e75cb5cf87
Pooranasundari <151390446+PooraniProjects@users.noreply.github.com>
2024-02-11 22:56:14 +0530

    Add files via upload

---
+++ portfolio page/assets/js/script.js
@@ -0,0 +1,18 @@
+
+/* ----- TYPING EFFECT ----- */
+let typeEffect= new Typed('.typeTxt',{
+    strings : ["Full Stack Developer"],
+    loop:true,
+    typeSpeed:110,
+    backSpeed:85,
+    backDelay:2000,
+   
+})
+let typeEect= new Typed('.typeText',{
+    strings : ["this is my short intro..."],
+    loop:true,
+    typeSpeed:110,
+    backSpeed:85,
+    backDelay:2000,
+   
+})
